0ba1972b70cfa08ec77060c71066cde18c3945fe
Lipon Roy <liponbdbd@gmail.com>
2022-03-26 17:10:46 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+	return (
+		<div className='App'>
+			<h1>Project Start</h1>
+			<h1>Pick Your Monitor</h1>
+		</div>
+	);
+}
+
+export default App;
